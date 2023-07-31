fd334dbe360f80d6d10c8dcb154df46d27fa9568
Yassine Khiadouch <yassine.uav@gmail.com>
2023-07-30 22:18:35 -0700

    add search homeProps

--- types/index.ts
+++ types/index.ts
@@ -4,29 +4,29 @@ export interface CustomButtonProps {
     title: string;
     containerStyles?: string;
     textStyles?:string;
-    rightIcon?: String;
+    rightIcon?: string;
     isDisable?: boolean;
     btnType?: "button" | "submit";
     handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export interface SearchManufacturerProps {
-    manufacturer: String;
-    setManufacturer: (manufacturer: String) => void;
+    manufacturer: string;
+    setManufacturer: (manufacturer: string) => void;
 }
 
 export interface CarProps {
     city_mpg: number;
-    class: String;
+    class: string;
     combination_mpg: number;
     cylinders: number;
     displacement: number;
-    drive: String;
-    fuel_type: String;
+    drive: string;
+    fuel_type: string;
     highway_mpg: number;
-    make: String;
-    model: String;
-    transmission: String;
+    make: string;
+    model: string;
+    transmission: string;
     year: number;
 }
 
@@ -38,7 +38,6 @@ export interface FilterProps {
     model : string;
 }
 
-
 export interface HomeProps {
     searchParams: FilterProps;
   }
@@ -48,7 +47,7 @@ export interface OptionProps {
     value: string;
 }
 export interface CustomFilterProps {
-    title: String;
+    title: string;
     options: OptionProps[];
 }
 
