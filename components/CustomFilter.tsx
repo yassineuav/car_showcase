20dcb8f8c46e525cdc723ce6fe3ca11bca2f22ae
Yassine Khiadouch <yassine.uav@gmail.com>
2023-07-30 19:17:31 -0700

    add search

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+'use client'
+import {useState, Fragment} from 'react'
+import Image from 'next/image'
+import {useRouter} from 'next/navigation'
+import {Listbox, Transition } from '@headlessui/react'
 
-const CustomFilter = () => {
+
+
+const CustomFilter = ({title, options}: any) => {
   return (
     <div>CustomFilter</div>
   )
